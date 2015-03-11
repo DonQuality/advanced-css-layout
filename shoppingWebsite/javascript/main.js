f2f167b61a92b2dcdd29b9e1593ced783001cc68
DonQuality <santosemmanuel@outlook.com>
2015-03-11 12:17:08 -0700

    advance css, grid and flex

--- shoppingWebsite/javascript/main.js
+++ shoppingWebsite/javascript/main.js
@@ -1,8 +1,8 @@
-var image, Ypos;
-function scroll(){
-  var image = document.getElementById('image');
-  var Ypos = window.pageYOffset;
-  image.style.top = window.pageYOffset * .4 +'px';
-}
+// var image, Ypos;
+// function scroll(){
+//   var image = document.getElementById('image');
+//   var Ypos = window.pageYOffset;
+//   image.style.top = window.pageYOffset * .4 +'px';
+// }
 
-window.addEventListener('scroll', scroll);
+// window.addEventListener('scroll', scroll);
