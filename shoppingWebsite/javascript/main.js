000eb5f054e9c961f5ee1cea9c69e4e3ba759b1a
DonQuality <santosemmanuel@outlook.com>
2015-03-12 08:44:53 -0700

    VW,responsive and flexbox

--- shoppingWebsite/javascript/main.js
+++ shoppingWebsite/javascript/main.js
@@ -1,8 +1,8 @@
-var image, Ypos;
-function scroll(){
-  var image = document.getElementById('image');
-  var Ypos = window.pageYOffset;
-  image.style.top = window.pageYOffset * .3 +'px';
-}
+// var image, Ypos;
+// function scroll(){
+//   var image = document.getElementById('image');
+//   var Ypos = window.pageYOffset;
+//   image.style.top = window.pageYOffset * .3 +'px';
+// }
 
-window.addEventListener('scroll', scroll);
+// window.addEventListener('scroll', scroll);
