331956d4009f8d7d2dd7396ce5aaab0281a04800
DonQuality <santosemmanuel@outlook.com>
2015-03-12 11:50:39 -0700

    transition effects

--- shoppingWebsite/javascript/main.js
+++ shoppingWebsite/javascript/main.js
@@ -23,3 +23,63 @@ $('#button').on('mouseleave', function(){
     'color':'white'
   });
 });
+// pinterest
+$('#pin').on('mouseenter', function(){
+  $(this).css({
+    'color':'red'
+  });
+});
+
+$('#pin').on('mouseleave', function(){
+  $(this).css({
+    'color':'white'
+  });
+});
+// instagram
+$('#instagram').on('mouseenter', function(){
+  $(this).css({
+    'color':'#967557'
+  });
+});
+
+$('#instagram').on('mouseleave', function(){
+  $(this).css({
+    'color':'white'
+  });
+});
+// facebook
+$('#facebook').on('mouseenter', function(){
+  $(this).css({
+    'color':'#3A589B'
+  });
+});
+
+$('#facebook').on('mouseleave', function(){
+  $(this).css({
+    'color':'white'
+  });
+});
+// google
+$('#google').on('mouseenter', function(){
+  $(this).css({
+    'color':'red'
+  });
+});
+
+$('#google').on('mouseleave', function(){
+  $(this).css({
+    'color':'white'
+  });
+});
+//mail
+$('#mail').on('mouseenter', function(){
+  $(this).css({
+    'color':'lime'
+  });
+});
+
+$('#mail').on('mouseleave', function(){
+  $(this).css({
+    'color':'white'
+  });
+});
